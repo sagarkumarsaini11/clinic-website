9d65a1fe7223bb21174e77423cc3e144671875f0
Manya <manysharma553@gmail.com>
2026-06-10 18:20:37 +0530

    correct hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,19 +1,46 @@
-import React from 'react'
-import './Hero.css'
-import Navbar from '../Navbar/Navbar'
-
+import React from 'react';
+import './Hero.css';
+import { FaArrowRight } from "react-icons/fa";
 
 const Hero = () => {
+
+  const scrollToAppointment = () => {
+    const section = document.getElementById("appointment");
+
+    if (section) {
+      section.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
     <div className='hero container'>
-   <div className='hero-text'>
-    <h1> We Ensures most Effective Treatment for their Specific Needs </h1>
-    <p>Krishna Advance Physio Clinic Leverages advance technology to deliver personalized physiotherapy care.</p>
-  
-   </div>
-      
-    </div>      
-  )
-}
+      <div className='hero-text'>
 
-export default Hero
+        <h1>
+          We Ensures most Effective Treatment for their Specific Needs
+        </h1>
+
+        <p>
+          Krishna Advance Physio Clinic Leverages advance technology to
+          deliver personalized physiotherapy care.
+        </p>
+
+        <button
+          className="appointment-btn"
+          onClick={scrollToAppointment}
+        >
+          <span>Book Appointment</span>
+
+          <div className="arrow-circle">
+            <FaArrowRight />
+          </div>
+        </button>
+
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
